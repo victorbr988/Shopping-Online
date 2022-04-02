905af757999c704899b057b931f1d96c0a2ada5c
victor gabriel <gabrielvghs666@gmail.com>
2022-04-01 22:24:58 -0300

    adiciona novas funcionalidades para a pagina

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 const getOl = document.querySelector('.items');
+
+// função que pega os dados digitados pelo usuário e passa como parâmetro para requisição da API
+const getButtonSearch = document.querySelector('#search');
+
+getButtonSearch.addEventListener('click', () => {
+  const getInput = document.querySelector('#search-items').value;
+  getInfoProducts(getInput);
+  getInput.innerHTML = '';
+  
+});
+
 // preciso pegar os items da API
-async function requestAPI() {
-  const API_URL = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=sapato')
+function requestAPI(search) {
+  const API_URL = fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
   .then((response) => response.json())
   .then((data) => data);
   return API_URL;
 }
+// adicionando texto de carregamento enquanto é feita a requisiçao da API
+function addloading() {
+  const createSectionLoading = document.createElement('section');
+  createSectionLoading.className = 'loading';
+  createSectionLoading.appendChild(createElementsHTML('h1', 'message', 'Carregando...'));
+  const getMain = document.querySelector('.userData');
+  getMain.appendChild(createSectionLoading);
+  return createSectionLoading;
+}
+// função que remove o elemento que contém a mensagem carregando
+function removeLoading() {
+  const getElement = document.querySelector('.loading');
+  getElement.remove();
+}
 // preciso pegar somente as informações dos produtos
-async function getInfoProducts() {
-  const products = await requestAPI();
+async function getInfoProducts(search) {
+  addloading()
+  const products = await requestAPI(search);
+  removeLoading()
   const getInfoItems = products.results;
   getInfoItems.forEach((item) => createElementItems(item));
   return products;
@@ -28,8 +55,8 @@ function createElementsHTML(element, classe, content) {
   return create;
 }
 // função que é executada após clicar em um dos items do carrinho
-function handleClick() {
-  console.log(getOl)
+function handleClick(item) {
+  item.remove();
 }
 // preciso adicionar o item ao carrinho quando clico no botão de adicionar
 function addItemInCart(image, name, price) {
@@ -37,7 +64,7 @@ function addItemInCart(image, name, price) {
   createLi.style.padding = '1rem';
 
   const createButton = createElementsHTML('button', 'btn', 'X')
-  createButton.addEventListener('click', handleClick)
+  createButton.addEventListener('click', () => handleClick(createLi));
 
   
   createLi.appendChild(createImageItem(image));
